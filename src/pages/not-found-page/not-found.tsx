d52f13537b1fbc89a3203c67130b8805a386c258
Imakando Sikamikami <imakandosikamikami@gmail.com>
2025-12-17 19:43:58 +0300

    Add files via upload

---
+++ src/pages/not-found-page/not-found.tsx
@@ -0,0 +1,11 @@
+function PageNotFound() {
+  return (
+    <div className="error-container">
+      <h1> 404 </h1>
+      <p>Oops! The page you're looking for is not here.</p>
+      <a href="#">Go Back to Home</a>
+    </div>
+  );
+}
+
+export { PageNotFound };
